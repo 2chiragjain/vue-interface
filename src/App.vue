b0b07724c2211c67349065152d9319994964326a
Jain, Chirag <chirag@usf.edu>
2025-08-29 11:07:22 -0400

    Additem and List

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="main-app" class="container" >
     <div class="row justify-content-center">
-      <appointmentlist :appointments="appointments" @remove="removeItem"/>
+      <AddAppointment @add="addItem"/>
+      <appointmentlist :appointments="appointments"
+       @remove="removeItem" @edit="editItem" />
     </div>
-    
-    
-
   </div>  
 </template>
 
@@ -14,25 +13,44 @@
 import axios from "axios"
 import Appointmentlist from "./components/AppointmentList";
 import _ from "lodash";
+import AddAppointment from "./components/AddAppointment.vue";
+
 export default {
   name: 'MainApp',
   data: function() {
     return {
       title: "Appointment List",
-      appointments: []
+      appointments: [],
+      aptIndex: 0
     };
   },
   components:{
-    Appointmentlist
+    Appointmentlist,
+    AddAppointment
   },
   mounted() {
     axios
     .get("/data/appointments.json")
-    .then(response => (this.appointments = response.data));
+    .then(response => (this.appointments = response.data.map(item=> {
+      item.aptID = this.aptIndex;
+      this.aptIndex++;
+      return item
+    })));
   },
   methods: {
     removeItem: function(apt){
       this.appointments=_.without(this.appointments,apt);
+    },
+    editItem: function(id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptID: id
+      });
+      this.appointments[aptIndex][field] = text;
+    },
+    addItem: function (apt) {
+      apt.aptID = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
     }
   }
 }
--- src/components/AppointmentList.vue
+++ src/components/AppointmentList.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="col-12 col-md-10 col-lg-7">
         <div class="list-group list-group-flush">
-            <div class="list-group-item d-flex align-items-start" v-for="(item,i) in appointments" v-bind:key="i">
+            <div class="list-group-item d-flex align-items-start" v-for="item in appointments" :key="item.aptindex">
                 <button class="mr-2 btn btn-sm btn-danger" @click="$emit('remove',item)">
                 <font-awesome-icon icon="trash" />
                 </button>
                 <div class="w-100">
                 <div class="d-flex justify-content-between">
-                    <span class="h4 text-primary">{{item.petName}}</span>
+                    <span class="h4 text-primary" 
+                        contenteditable="true" 
+                        @blur="$emit('edit', item.aptID, 'petName', $event.target.innerText)">
+                        {{item.petName}}</span>
                     <span class="float-right">{{formattedDate(item.aptDate)}}</span>
                 </div>
                 <div class="owner-name">
-                    <span class="font-weight-bold text-primary mr-1">Owner:</span>
-                    <span>{{item.petOwner}}</span>
+                    <span class="font-weight-bold text-primary mr-1" >Owner:</span>
+                    <span 
+                        contenteditable="true" 
+                        @blur="$emit('edit', item.aptID, 'petOwner', $event.target.innerText)">{{item.petOwner}}</span>
                 </div>
-                <div>{{item.aptNotes}}</div>
+                <div 
+                    contenteditable="true" 
+                    @blur="$emit('edit', item.aptID, 'aptNotes', $event.target.innerText)">{{item.aptNotes}}</div>
                 </div>
             </div>
         </div>
